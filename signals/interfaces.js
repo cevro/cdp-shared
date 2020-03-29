2897b0c5bfa8d851f17d660329e0673315f3926f
Michal "mišo" Červeňák <miso@fykos.cz>
2020-03-29 04:14:12 +0200

    add gitignore

--- signals/interfaces.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const signalTypes_1 = require("@app/consts/signals/signalTypes");
-exports.ABLights = ['HZ', 'Z', 'C'];
-exports.ABDef = {
-    type: signalTypes_1.signalTypes.TYPE_AB,
-    lights: exports.ABLights,
-};
